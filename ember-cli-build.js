523d03b29c65a17971ce04584b68ee1a167d1ea9
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-08-02 15:46:35 -0400

    Try to use Vite

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -10,21 +10,7 @@ module.exports = function (defaults) {
     },
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-  const { Webpack } = require('@embroider/webpack');
-
-  return require('@embroider/compat').compatBuild(app, Webpack, {
+  return require('@embroider/compat').compatBuild(app, undefined, {
     extraPublicTrees: [],
     staticAddonTrees: true,
     staticAddonTestSupportTrees: true,
@@ -33,6 +19,7 @@ module.exports = function (defaults) {
     staticComponents: true,
     splitControllers: true,
     splitRouteClasses: true,
+    staticEmberSource: true,
     // splitAtRoutes: ['route.name'], // can also be a RegExp
     // packagerOptions: {
     //    webpackConfig: { }
